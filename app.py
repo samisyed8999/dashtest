d50f20aef59db590aa897ccce06347df8ce3753a
pabsyu <65567874+pabsyu@users.noreply.github.com>
2020-05-18 22:08:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,60 +4,79 @@ import dash_html_components as html
 import plotly.graph_objs as go
 
 ########### Define your variables
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
-abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='lightblue'
-color2='darkgreen'
-mytitle='Beer Comparison'
-tabtitle='beer!'
-myheading='Flying Dog Beers'
-label1='IBU'
-label2='ABV'
-githublink='https://github.com/austinlasseter/flying-dog-beers'
-sourceurl='https://www.flyingdog.com/beers/'
+# beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
+# ibu_values=[35, 60, 85, 75]
+# abv_values=[5.4, 7.1, 9.2, 4.3]
+# color1='lightblue'
+# color2='darkgreen'
+# mytitle='Beer Comparison'
+# tabtitle='beer!'
+# myheading='Flying Dog Beers'
+# label1='IBU'
+# label2='ABV'
+# githublink='https://github.com/austinlasseter/flying-dog-beers'
+# sourceurl='https://www.flyingdog.com/beers/'
 
 ########### Set up the chart
-bitterness = go.Bar(
-    x=beers,
-    y=ibu_values,
-    name=label1,
-    marker={'color':color1}
-)
-alcohol = go.Bar(
-    x=beers,
-    y=abv_values,
-    name=label2,
-    marker={'color':color2}
-)
+# bitterness = go.Bar(
+#     x=beers,
+#     y=ibu_values,
+#     name=label1,
+#     marker={'color':color1}
+# )
+# alcohol = go.Bar(
+#     x=beers,
+#     y=abv_values,
+#     name=label2,
+#     marker={'color':color2}
+# )
 
-beer_data = [bitterness, alcohol]
-beer_layout = go.Layout(
-    barmode='group',
-    title = mytitle
-)
+# beer_data = [bitterness, alcohol]
+# beer_layout = go.Layout(
+#     barmode='group',
+#     title = mytitle
+# )
 
-beer_fig = go.Figure(data=beer_data, layout=beer_layout)
+# beer_fig = go.Figure(data=beer_data, layout=beer_layout)
 
 
 ########### Initiate the app
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__)
 server = app.server
-app.title=tabtitle
+#app.title=tabtitle
 
-########### Set up the layout
-app.layout = html.Div(children=[
-    html.H1(myheading),
-    dcc.Graph(
-        id='flyingdog',
-        figure=beer_fig
+# ########### Set up the layout
+# app.layout = html.Div(children=[
+#     html.H1(myheading),
+#     dcc.Graph(
+#         id='flyingdog',
+#         figure=beer_fig
+#     ),
+#     html.A('Code on Github', href=githublink),
+#     html.Br(),
+#     html.A('Data Source', href=sourceurl),
+#     ]
+# )
+app.layout = html.Div([
+    html.Div(
+        html.H1('My Dashboard')
     ),
-    html.A('Code on Github', href=githublink),
-    html.Br(),
-    html.A('Data Source', href=sourceurl),
-    ]
-)
+
+    html.Div([
+        dcc.Input(id="stock-input", value=ticker, type="text"),
+        html.Button(id="submit-button", n_clicks=0, children="Submit")
+    ]),
+
+
+    dcc.Tabs(id="tabs", value='Tab1', children=[
+        dcc.Tab(label='Income Statement', id='tab1', value= 'Tab1', children=[]),
+        dcc.Tab(label='Balance Sheet', id='tab2', value= 'Tab2', children=[]),
+        dcc.Tab(label='Cash Flow Statement', id='tab3', value= 'Tab3', children=["yo"]),
+        dcc.Tab(label='Intrinsic Value estimations', id='tab4', value= 'Tab4', children=["yo"]),
+
+    ])
+])
 
 if __name__ == '__main__':
     app.run_server()
