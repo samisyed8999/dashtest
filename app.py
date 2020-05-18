c8cb71487df0507671d3c4727869ef24127c8ae9
pabsyu <65567874+pabsyu@users.noreply.github.com>
2020-05-18 22:40:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,27 @@ app = dash.Dash()
 server = app.server
 
 
-app.layout = html.Div(
-    html.Div(
-        html.H1(children='My Dashboard'))
-)
+app = dash.Dash(__name__)
+server = app.server
 
+app.layout = html.Div([
+    html.Div(
+        html.H1('My Dashboard')
+    ),
+
+    html.Div([
+        dcc.Input(id="stock-input", value="AAPL", type="text"),
+        html.Button(id="submit-button", n_clicks=0, children="Submit")
+    ]),
+
+
+    dcc.Tabs(id="tabs", value='Tab1', children=[
+        dcc.Tab(label='Income Statement', id='tab1', value= 'Tab1', children=["Hello"]),
+        dcc.Tab(label='Balance Sheet', id='tab2', value= 'Tab2', children=["lol"]),
+        dcc.Tab(label='Cash Flow Statement', id='tab3', value= 'Tab3', children=["yo"]),
+        dcc.Tab(label='Intrinsic Value estimations', id='tab4', value= 'Tab4', children=["yo"]),
+    ])
+])
 
 
 if __name__ == '__main__':
