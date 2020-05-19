8a8bbedd52eaf199b91009c320799ebc229899c7
pabsyu <65567874+pabsyu@users.noreply.github.com>
2020-05-19 08:01:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ from simfin.names import *
 import dash_table
 from dash.dependencies import Output, Input, State
 
-tabtitle = 'Financial Statements'
+#tabtitle = 'Financial Statements'
+app.title = ("Financial Statements")
 sf.set_data_dir('~/simfin_data/')
 api_key="ZxGEGRnaTpxMF0pbGQ3JLThgqY2HBL17"
 
