6b9819aa77ff613ccbb2ec4ac2c2b7b34029878d
samisyed8999 <65790412+samisyed8999@users.noreply.github.com>
2020-05-24 03:34:00 +0100

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,3 +1,12 @@
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+import pandas as pd
+import simfin as sf
+from simfin.names import *
+import dash_table
+from dash.dependencies import Output, Input, State
+
 def callbacks():
 
     @app.callback(
