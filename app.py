65059dd6ccf7ad3f3417f32f89ea0430ee1cbf55
samisyed8999 <65790412+samisyed8999@users.noreply.github.com>
2020-05-24 04:02:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -127,7 +127,65 @@ app.layout = html.Div([
     ])
 ])
 
+def callbacks():
 
+    @app.callback(
+    Output('table', 'data'),
+    [Input("submit-button", "n_clicks")],
+    [State("stock-input", "value")])
+    def update_data(n_click, input_value):
+        df1 = df_income.loc[input_value]
+        data = df1.to_dict("records")
+        return data
+
+    @app.callback(
+    Output('table', 'columns'),
+    [Input("submit-button", "n_clicks")],
+    [State("stock-input", "value")])
+    def update_columns(n_click, input_value):
+            df1 = df_income.loc[input_value]
+            columns =[{"name": i, "id": i} for i in df1.columns]
+            return columns
+
+
+    @app.callback(
+    Output('table2', 'data'),
+    [Input("submit-button", "n_clicks")],
+    [State("stock-input", "value")])
+    def update_data(n_click, input_value):
+        df2 = df_signals.loc[input_value]
+        data = df2.to_dict("records")
+        return data
+
+    @app.callback(
+    Output('table2', 'columns'),
+    [Input("submit-button", "n_clicks")],
+    [State("stock-input", "value")])
+    def update_columns(n_click, input_value):
+            df2 = df_signals.loc[input_value]
+            columns =[{"name": i, "id": i} for i in df2.columns]
+            return columns
+
+
+    @app.callback(
+    Output('table3', 'data'),
+    [Input("submit-button", "n_clicks")],
+    [State("stock-input", "value")])
+    def update_data(n_click, input_value):
+        df3 = df_balance.loc[input_value]
+        data = df3.to_dict("records")
+        return data
+
+    @app.callback(
+    Output('table3', 'columns'),
+    [Input("submit-button", "n_clicks")],
+    [State("stock-input", "value")])
+    def update_columns(n_click, input_value):
+            df3 = df_balance.loc[input_value]
+            columns =[{"name": i, "id": i} for i in df3.columns]
+            return columns
+        
+callbacks() 
 
 if __name__ == '__main__':
     app.run_server()
