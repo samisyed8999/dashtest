236e47ff5aec607ff660dbb99eb5e6bac55d37a5
samisyed8999 <65790412+samisyed8999@users.noreply.github.com>
2020-09-02 20:46:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,22 +11,22 @@ from dash.dependencies import Output, Input, State
 sf.set_data_dir('~/simfin_data/')
 api_key="ZxGEGRnaTpxMF0pbGQ3JLThgqY2HBL17"
 
-df_cashflow = sf.load_cashflow(variant='annual', market='us', index=[TICKER])
+df_cashflow = sf.load_cashflow(variant='annual', market='us', index=[TICKER, FISCAL YEAR])
 df_freecashflow = pd.DataFrame()
-df_freecashflow['Year'] = df_cashflow['Fiscal Year'].copy()
+#df_freecashflow['Year'] = df_cashflow['Fiscal Year'].copy()
 df_freecashflow['FCF'] = df_cashflow[NET_CASH_OPS] + df_cashflow[CAPEX]
 df_freecashflow['FCF']= df_freecashflow['FCF'].apply(lambda x: x / 1000000)
-graph = df_freecashflow.loc['AAPL']
-graph = graph.transpose()
+transposed_table = df_freecashflow.loc['AAPL']
+transposed_table = transposed_table.transpose()
+# for some reason transpose just does not work, take out transpose and there are two columns year
+# and cash flow but for some reason when transposed the output is the same for each column. 
 
 app = dash.Dash(__name__)
 server = app.server
 app.layout = html.Div([
 
     dcc.Tabs(id='tabs', value='tab-1', children=[
-        dcc.Tab(label='Income Statement', value='tab-1'),
-        dcc.Tab(label='Balance Sheet', value='tab-2'),
-        dcc.Tab(label='Cash Flow Statement', value='tab-3'),
+        dcc.Tab(label='Cash Flow Statement', value='tab-1'),
 
     ]),
 
@@ -40,12 +40,12 @@ def render_content(tab):
     if tab == 'tab-1':
         return html.Div([
             html.Div(
-                html.H3('Income statement (m)')
+                html.H3('Cashflow Statement (m)')
             ),
             dash_table.DataTable(
                 id='table',
-                columns=[{"name": i, "id": i} for i in graph.columns],
-                data=graph.to_dict('records'),
+                columns=[{"name": i, "id": i} for i in transposed_table.columns],
+                data=transposed_table.to_dict('records'),
             )
         ])
 if __name__ == '__main__':
